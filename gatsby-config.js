79a56d9f53fa56f93383c96dde461e46812fe7f9
Aravind Jaimon <aravindjaimon1@gmail.com>
2020-08-25 12:30:04 +0530

    cleanup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Aravind Jaimon`,
+    description: `Aravind Jaimon is an Experienced FrontEnd Developer based in India, Who can create stunning websites`,
+    author: `@aravindjaimon`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -21,14 +21,9 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        // icon: `src/images/gatsby-icon.png`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
-}
+};
